854829826ebecf20fa7522e59acf5052576b6663
罗泽霖 <luozelin1742@ichinae.com>
2021-11-08 16:21:07 +0800

    流畅的Python

---
+++ FluentPython/20211108/p1_Card.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-#
+# ----------------------
+# Name: p1
+# Description: 一摞卡片
+# Author: luozelin
+# Date: 2021/11/8 0008
+# ----------------------
+
+import collections
+
+Card = collections.namedtuple('Card',['rank','suit'])	# 命名元组
+
+class FrenchDeck:
+	# 扑克点数
+	ranks = [str(n) for n in range(2,11)] + list('JQKA')
+	# 花色
+	suits = 'spades diamonds clubs hearts'.split(' ')
+
+	def __init__(self):
+		self._cards = [Card(rank,suit) for suit in self.suits for rank in self.ranks]
+
+	def __len__(self):
+		return len(self._cards)
+
+	def __getitem__(self, position):
+		return self._cards[position]
+
+
+if __name__=='__main__':
+	obj = FrenchDeck()
+	print(f'查看卡牌当中的某一个牌{obj.__getitem__(1)}')
+	print(f'查看所有的卡牌：{obj._cards}')
+	print(f'查看某一个元组对象中的值:{obj.__getitem__(1).rank}')
